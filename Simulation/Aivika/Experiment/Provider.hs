e04bba4aa39f744d673b965df2109d4ddfa8615d
David Sorokin <david.sorokin@gmail.com>
2017-03-29 08:52:39 +0300

    Initial import.

---
+++ Simulation/Aivika/Experiment/Provider.hs
@@ -0,0 +1,19 @@
+
+-- |
+-- Module     : Simulation.Aivika.Experiment.Provider
+-- Copyright  : Copyright (c) 2017, David Sorokin <david.sorokin@gmail.com>
+-- License    : AllRightsReserved
+-- Maintainer : David Sorokin <david.sorokin@gmail.com>
+-- Stability  : experimental
+-- Tested with: GHC 8.0.2
+--
+-- This module re-exports the library functionality.
+--
+
+module Simulation.Aivika.Experiment.Provider
+       (-- * Modules
+        module Simulation.Aivika.Experiment.Provider.ExperimentAgent,
+        module Simulation.Aivika.Experiment.Provider.ExperimentAggregator) where
+
+import Simulation.Aivika.Experiment.Provider.ExperimentAgent
+import Simulation.Aivika.Experiment.Provider.ExperimentAggregator
---
+++ Simulation/Aivika/Experiment/Provider/ExperimentAgent.hs
@@ -0,0 +1,14 @@
+
+-- |
+-- Module     : Simulation.Aivika.Experiment.Provider.ExperimentAgent
+-- Copyright  : Copyright (c) 2017, David Sorokin <david.sorokin@gmail.com>
+-- License    : AllRightsReserved
+-- Maintainer : David Sorokin <david.sorokin@gmail.com>
+-- Stability  : experimental
+-- Tested with: GHC 8.0.2
+--
+-- This module defines an experiment agent that further processes the simulation results,
+-- for example, saving them in the database.
+--
+
+module Simulation.Aivika.Experiment.Provider.ExperimentAgent where
